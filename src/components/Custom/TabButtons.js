0768733ae81ca0dc2eaf82d9929a6c3299abdfe5
Ismail Hossain <mdismail645221@gmail.com>
2023-01-10 16:06:55 +0600

    custom tabs styled added

--- src/components/Custom/TabButtons.js
+++ src/components/Custom/TabButtons.js
@@ -3,8 +3,15 @@ import { styled, Box, Tabs, Tab } from '@mui/material';
 
 
 
-export const CustomTabs = styled(Box)(({ theme }) => ({
-   
+const CustomTabs = styled(Tabs)(({ theme }) => ({
+    padding: '5px',
+    fontWeight: '700',
+   '& .css-1wf8b0h-MuiTabs-flexContainer ': {
+     justifyContent: 'space-between',
+     maxWidth: '600px',
+     margin: '0 auto'
+
+   }
 }))
 
 
@@ -17,11 +24,11 @@ export default function TabButton({value, setValue}) {
 
   return (
     <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      <Tabs value={value} onChange={handleChange} centered>
+      <CustomTabs value={value} onChange={handleChange} centered>
         <Tab label="Item One" />
         <Tab label="Item Two" />
         <Tab label="Item Three" />
-      </Tabs>
+      </CustomTabs>
     </Box>
   );
 }
